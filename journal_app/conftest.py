e0ee3e4ab32812464a7ee3648fb7538314f43edb
CJ Stein <cjstein@resume.com>
2020-09-27 21:04:06 -0500

    Initi commit after django-cookiecutter

---
+++ journal_app/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from journal_app.users.models import User
+from journal_app.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user() -> User:
+    return UserFactory()
