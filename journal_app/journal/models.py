afccbf4bee6d99a67984273bcf2eb48c56720690
CJ <resume@cjstein.com>
2020-09-30 20:26:20 -0500

    updated get_absolute_url on models

--- journal_app/journal/models.py
+++ journal_app/journal/models.py
@@ -2,6 +2,7 @@
 import uuid
 # Django imports
 from django.db import models
+from django.urls import reverse
 # Custom imports
 from journal_app.users.models import User
 
@@ -23,7 +24,9 @@ class Contact(TimeStampedModel):
     Contact model will be user supplied and all journal Entries that are tagged with that contact or "public" will be
     filtered when released.
     """
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
+    uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     # Fields below will only need at least one filled out
     email = models.EmailField(blank=True)
     password = models.CharField(max_length=100, blank=True)
@@ -32,11 +35,20 @@ class Contact(TimeStampedModel):
     # Possibly add Telegrambot
     # Possibly add signal bot
 
+    def get_absolute_url(self):
+        """Get url for user's detail view.
+
+        Returns:
+            str: URL for user detail.
+
+        """
+        return reverse("journal:contact_detail", kwargs={"uuid": self.uuid})
+
     def __str__(self):
         return self.name
 
     def __repr__(self):
-        return f'{self.__class__.__name__}({self.name}, {self.email}, {self.password})'
+        return f'{self.__class__.__name__}({self.user}, {self.name})'
 
 
 class Entry(TimeStampedModel):
@@ -52,6 +64,15 @@ class Entry(TimeStampedModel):
     # If Public is flagged as True, when the posts get released, these entries will be viewable by all contacts.
     public = models.BooleanField(default=False)
     contact = models.ManyToManyField(Contact)
+
+    def get_absolute_url(self):
+        """Get url for user's detail view.
+
+        Returns:
+            str: URL for user detail.
+
+        """
+        return reverse("journal:entry_detail", kwargs={"uuid": self.uuid})
 
     def __str__(self):
         return self.title
--- journal_app/journal/urls.py
+++ journal_app/journal/urls.py
@@ -5,13 +5,13 @@ app_name = "journal"
 
 urlpatterns = [
     # Entry urls
-    path("", EntryListView.as_view(), name='entry_list_view'),
-    path("entry/<uuid:uuid>", EntryDetailView.as_view(), name='entry_detail_view'),
-    path("entry/new", EntryCreateView.as_view(), name='entry_create_view'),
-    path("entry/<uuid:uuid>/update", EntryUpdateView.as_view(), name='entry_update_view'),
+    path("", EntryListView.as_view(), name='entry_list'),
+    path("entry/<uuid:uuid>", EntryDetailView.as_view(), name='entry_detail'),
+    path("entry/new", EntryCreateView.as_view(), name='entry_create'),
+    path("entry/<uuid:uuid>/update", EntryUpdateView.as_view(), name='entry_update'),
     # Contact urls
-    path("contacts", ContactListView.as_view(), name='contact_list_view'),
-    path("contact/<uuid:uuid>", ContactDetailView.as_view(), name='contact_detail_view'),
-    path("contact/new", ContactCreateView.as_view(), name='contact_create_view'),
-    path("contact/<uuid:uuid>/update", ContactUpdateView.as_view(), name='contact_update_view'),
+    path("contacts", ContactListView.as_view(), name='contact_list'),
+    path("contact/<uuid:uuid>", ContactDetailView.as_view(), name='contact_detail'),
+    path("contact/new", ContactCreateView.as_view(), name='contact_create'),
+    path("contact/<uuid:uuid>/update", ContactUpdateView.as_view(), name='contact_update'),
 ]
