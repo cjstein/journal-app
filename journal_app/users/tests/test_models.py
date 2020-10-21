13732aa31d9ea4ffcf5a9c84ff3b43f0cba0aacd
CJ Stein <cjstein@resume.com>
2020-10-21 11:22:22 -0500

    Added tests for updated model fields

--- journal_app/users/tests/test_models.py
+++ journal_app/users/tests/test_models.py
@@ -1,4 +1,5 @@
 import pytest
+from django.utils import timezone
 
 from journal_app.users.models import User
 
@@ -7,3 +8,16 @@ pytestmark = pytest.mark.django_db
 
 def test_user_get_absolute_url(user: User):
     assert user.get_absolute_url() == f"/users/{user.username}/"
+
+
+def test_user_checkin_deadline(user: User):
+    user.last_checkin = timezone.now()
+    assert user.checkin_deadline == user.last_checkin + timezone.timedelta(days=user.days_to_release)
+    assert user.checkin_deadline != user.last_checkin + timezone.timedelta(days=1000)
+
+
+def test_release_entries(user: User):
+    user.last_checkin = timezone.now()
+    assert not user.release_entries
+    user.last_checkin = timezone.now() - timezone.timedelta(days=user.days_to_release + 1)
+    assert user.release_entries
