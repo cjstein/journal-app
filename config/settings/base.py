08585468e2308837e54f6b7c4aa360ab0d11adc5
CJ <resume@cjstein.com>
2020-11-02 20:24:04 -0600

    fixing email subject prefix

--- config/settings/base.py
+++ config/settings/base.py
@@ -285,7 +285,7 @@ CELERY_TASK_SOFT_TIME_LIMIT = 60
 CELERY_BEAT_SCHEDULER = "django_celery_beat.schedulers:DatabaseScheduler"
 # django-allauth
 # ------------------------------------------------------------------------------
-ACCOUNT_EMAIL_SUBJECT_PREFIX = None
+ACCOUNT_EMAIL_SUBJECT_PREFIX = ""
 ACCOUNT_ALLOW_REGISTRATION = env.bool("DJANGO_ACCOUNT_ALLOW_REGISTRATION", True)
 # https://django-allauth.readthedocs.io/en/latest/configuration.html
 ACCOUNT_AUTHENTICATION_METHOD = "username_email"
