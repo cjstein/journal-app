c818192c555170675c8f848fb91f05dbafbda655
CJ Stein <cjstein@resume.com>
2020-11-23 09:29:36 -0600

    Fixed views for subscription issue

--- journal_app/subscription/views.py
+++ journal_app/subscription/views.py
@@ -15,11 +15,8 @@ from journal_app.users.models import User
 
 @login_required
 def home(request):
-    try:
-        return render(request, 'subscription/home.html', get_subscription_status(request.user))
+    return render(request, 'subscription/home.html', get_subscription_status(request.user))
 
-    except StripeCustomer.DoesNotExist:
-        return render(request, 'subscription/home.html')
 
 
 @csrf_exempt
