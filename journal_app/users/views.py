2c6a65d6b24ccaf700f85acb7b358d5824760475
CJ Stein <cjstein@resume.com>
2020-11-29 13:47:36 -0600

    added utility to check whether user owns

--- journal_app/users/views.py
+++ journal_app/users/views.py
@@ -1,17 +1,18 @@
 from django.contrib import messages
 from django.contrib.auth import get_user_model
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.template.defaultfilters import date
 from django.urls import reverse, reverse_lazy
 from django.utils import timezone
 from django.views.generic import DetailView, RedirectView, UpdateView
 
 from journal_app.journal.models import Entry
+from journal_app.users.utils import user_viewing_own_profile
 
 User = get_user_model()
 
 
-class UserDetailView(LoginRequiredMixin, DetailView):
+class UserDetailView(UserPassesTestMixin, LoginRequiredMixin, DetailView):
 
     model = User
 
@@ -23,11 +24,14 @@ class UserDetailView(LoginRequiredMixin, DetailView):
         context['user'] = self.get_object()
         return context
 
+    def test_func(self):
+        return user_viewing_own_profile(self.request, self.kwargs['username'])
+
 
 user_detail_view = UserDetailView.as_view()
 
 
-class UserUpdateView(LoginRequiredMixin, UpdateView):
+class UserUpdateView(UserPassesTestMixin, LoginRequiredMixin, UpdateView):
 
     model = User
     fields = ["name"]
@@ -44,6 +48,9 @@ class UserUpdateView(LoginRequiredMixin, UpdateView):
         )
         return super().form_valid(form)
 
+    def test_func(self):
+        return user_viewing_own_profile(self.request, self.kwargs['username'])
+
 
 user_update_view = UserUpdateView.as_view()
 
@@ -59,7 +66,7 @@ class UserRedirectView(LoginRequiredMixin, RedirectView):
 user_redirect_view = UserRedirectView.as_view()
 
 
-class UserCheckinView(LoginRequiredMixin, RedirectView):
+class UserCheckinView(UserPassesTestMixin, LoginRequiredMixin, RedirectView):
     url = reverse_lazy('journal:entry_list')
 
     def get_object(self):
@@ -75,6 +82,9 @@ class UserCheckinView(LoginRequiredMixin, RedirectView):
             f'Thanks for checking in. Your next deadline is {date(user.checkin_deadline, "SHORT_DATE_FORMAT")}.'
         )
         return super().get_redirect_url(*args, **kwargs)
+
+    def test_func(self):
+        return user_viewing_own_profile(self.request, self.kwargs['username'])
 
 
 user_checkin_view = UserCheckinView.as_view()
@@ -105,7 +115,7 @@ class AnonUserCheckinView(RedirectView):
 anon_user_checkin_view = AnonUserCheckinView.as_view()
 
 
-class RetractPosts(LoginRequiredMixin, RedirectView):
+class RetractPosts(UserPassesTestMixin, LoginRequiredMixin, RedirectView):
 
     def get_object(self):
         return User.objects.get(username=self.request.user.username)
@@ -126,11 +136,14 @@ class RetractPosts(LoginRequiredMixin, RedirectView):
         user.save()
         return reverse("users:detail", kwargs={'username': self.request.user.username})
 
+    def test_func(self):
+        return user_viewing_own_profile(self.request, self.kwargs['username'])
+
 
 retract_posts_view = RetractPosts.as_view()
 
 
-class ProfileSettings(LoginRequiredMixin, UpdateView):
+class ProfileSettings(UserPassesTestMixin, LoginRequiredMixin, UpdateView):
     model = User
     fields = ['days_to_release_setting']
     template_name = "users/user_settings_form.html"
@@ -144,6 +157,9 @@ class ProfileSettings(LoginRequiredMixin, UpdateView):
             self.request, messages.SUCCESS, "Information successfully updated"
         )
         return super().form_valid(form)
+
+    def test_func(self):
+        return user_viewing_own_profile(self.request, self.kwargs['username'])
 
 
 settings_update_view = ProfileSettings.as_view()
