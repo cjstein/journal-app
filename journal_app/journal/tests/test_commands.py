cdf514bc82af6bf697feb994e95b3bfd7f58c777
CJ Stein <cjstein@resume.com>
2020-12-05 17:50:45 -0600

    changed trial to not create Stripe customer

--- journal_app/journal/tests/test_commands.py
+++ journal_app/journal/tests/test_commands.py
@@ -5,6 +5,7 @@ from django.core.management import call_command
 from django.test import TestCase
 
 from journal_app.journal.tests.factories import ContactFactory, EntryFactory
+from journal_app.journal_mail.models import Mail
 from journal_app.users.tests.test_views import REFERENCE_DATE
 
 pytestmark = pytest.mark.django_db
@@ -13,6 +14,9 @@ pytestmark = pytest.mark.django_db
 class TestReleaseEntries(TestCase):
 
     def setUp(self):
+        """
+        Settings up two different users and testing release of one Users set of entries
+        """
         self.entry1 = EntryFactory()
         self.entry2 = EntryFactory()
         self.contact = ContactFactory()
@@ -20,6 +24,9 @@ class TestReleaseEntries(TestCase):
         self.user2 = self.entry2.user
 
     def call_command(self, *args, **kwargs):
+        """
+        This calls the release entry command.  This command
+        """
         out = StringIO()
         call_command(
             'release_entries',
@@ -31,6 +38,9 @@ class TestReleaseEntries(TestCase):
         return out.getvalue()
 
     def test_release_entrires(self):
+        """
+        Check initialization state of users
+        """
         assert self.user1.release_entries is False
         assert self.entry1.released is False
         assert self.user2.release_entries is False
