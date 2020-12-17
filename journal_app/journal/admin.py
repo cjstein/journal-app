ee40e2685093ee9ca8dcacbc189ad77b04aa84d5
CJ Stein <cjstein@resume.com>
2020-12-17 09:35:04 -0600

    added more content to admin

--- journal_app/journal/admin.py
+++ journal_app/journal/admin.py
@@ -2,6 +2,57 @@ from django.contrib import admin
 
 from journal_app.journal.models import Contact, Entry
 
-# Register your models here.
-admin.site.register(Entry)
-admin.site.register(Contact)
+
+@admin.register(Entry)
+class EntryAdmin(admin.ModelAdmin):
+    list_display = (
+        'title',
+        'user',
+        'created',
+        'released',
+    )
+
+    sortable_by = (
+        'user',
+        'created',
+    )
+
+    search_fields = (
+        'title',
+        'uuid',
+        'user',
+        'contact',
+    )
+
+    list_filter = (
+        'user',
+        'released',
+        'public',
+    )
+
+
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'user',
+        'uuid',
+        'email',
+        'phone',
+    )
+
+    sortable_by = (
+        'name',
+    )
+
+    search_fields = (
+        'uuid',
+        'user',
+        'name',
+        'email',
+        'phone'
+    )
+
+    list_filter = (
+        'user',
+    )
