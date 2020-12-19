fef1eb5001f68d8de61f19a8b6435bdcfa484d78
CJ Stein <cjstein@resume.com>
2020-12-19 12:34:29 -0600

    created list admin

--- journal_app/subscription/admin.py
+++ journal_app/subscription/admin.py
@@ -2,4 +2,28 @@ from django.contrib import admin
 
 from journal_app.subscription.models import StripeCustomer
 
-admin.site.register(StripeCustomer)
+
+@admin.register(StripeCustomer)
+class StripeCustomerAdmin(admin.ModelAdmin):
+    list_display = [
+        'user',
+        'status',
+        'subscription_end',
+    ]
+
+    sortable_by = [
+        'user',
+        'subscription_end',
+    ]
+
+    list_filter = [
+        'product_name',
+        'status',
+    ]
+
+    search_fields = [
+        'user',
+        'product_name',
+        'stripe_customer_id',
+        'stripe_subscription_id',
+    ]
