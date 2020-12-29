717860fd2fd3c72c020c85649f1db24a4015be90
CJ Stein <33003756+cjstein@users.noreply.github.com>
2020-12-29 12:31:46 -0600

    Create utilities.py

---
+++ journal_app/journal_mail/utilities.py
@@ -0,0 +1,18 @@
+import requests
+import json
+
+
+
+def shortener(link):
+    api_link = 'https://api-ssl.bitly.com/v4/shorten'
+    headers = {
+        'Authorization': f'Bearer {cf.BITLY_TOKEN}',
+        'Content-Type': 'application/json',
+    }
+    data = {
+        "long_url": f'{link}',
+
+    }
+    response = requests.post(api_link, headers=headers, data=json.dumps(data))
+    returned_data = json.loads(response.text)
+    return returned_data['id']
