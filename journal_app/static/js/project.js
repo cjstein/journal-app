417ca70f3da3af01a6b70dfb934c8487385eed6d
CJ <resume@cjstein.com>
2021-01-13 20:06:04 -0600

    delete button now redirects to contact table

--- journal_app/static/js/project.js
+++ journal_app/static/js/project.js
@@ -1,7 +1,8 @@
 function ContactDeleteConfirmAction(msg, pk){
     // triggers a popup to make sure user is ok with a destructive operation
     if(confirm(msg)){
-        fetch("/journal/contact/" + pk + "/delete/").then(r => location.reload());
+        window.location.replace("/journal/contact/" + pk + "/delete/")
+        // fetch("/journal/contact/" + pk + "/delete/").then(r => location.reload());
     } else {
         return false;
     }
