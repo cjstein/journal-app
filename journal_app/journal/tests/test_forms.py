f57acaf2e75c7903867abc8c08c3c03b4bc0ff1a
CJ Stein <cjstein@resume.com>
2021-01-25 20:50:36 -0600

    fixed tests

--- journal_app/journal/tests/test_forms.py
+++ journal_app/journal/tests/test_forms.py
@@ -15,10 +15,17 @@ def _create_form(values):
 
 
 def test_contact_form_validation_requires_name():
-    values = ('', 'cj@journal.com', '222-333-4444')
+    values = ('', 'cj@journal.com', '2223334444')
     form = _create_form(values)
     assert not form.is_valid()
     assert form.errors == {'name': ['This field is required.']}
+
+
+def test_contact_form_validation_bad_number():
+    values = ('Bad Number', 'cj@journal.com', '222-333-4444')
+    form = _create_form(values)
+    assert not form.is_valid()
+    assert form.errors == {'phone': ['Phone number must be 10 digits long with no spaces, dashes, or parenthesis']}
 
 
 def test_contact_form_validation_needs_at_least_one_contact_field():
@@ -33,7 +40,7 @@ def test_contact_form_validation_needs_at_least_one_contact_field():
 
 @pytest.mark.parametrize("email, phone", [
     ('cj@journal.com', ''),
-    ('', '222-333-4444'),
+    ('', '2223334444'),
 ])
 def test_contact_form_validation_passes_with_one_contact_field(
         email, phone):
