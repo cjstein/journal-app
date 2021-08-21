a58a7a70cf04ebe9937be34636413ae36b97b215
CJ <resume@cjstein.com>
2021-08-21 12:58:25 -0500

    created JS and HTML to get the price ID

--- journal_app/static/js/stripe.js
+++ journal_app/static/js/stripe.js
@@ -2,31 +2,26 @@
 
 console.log("Sanity check!");
 
+function checkout(price) {
 // Get Stripe publishable key
-fetch("/subscription/config/")
-    .then((result) => { return result.json(); })
-    .then((data) => {
-        // Initialize Stripe.js
-        const stripe = Stripe(data.publicKey);
-
-        // new
-        // Event handler for Annual Subscription
-        let Btn = document.querySelector(".stripe-button");
-        if (Btn !== null) {
-            Btn.addEventListener("click", () => {
-                var price = Btn.id
-                // Get Checkout Session ID
-                fetch("/subscription/create-checkout-session/?price=" + price)
-                    .then((result) => { return result.json(); })
-                    .then((data) => {
-                        console.log(data);
-                        // Redirect to Stripe Checkout
-                        return stripe.redirectToCheckout({sessionId: data.sessionId})
-                    })
-                    .then((res) => {
-                        console.log(res);
-                    });
-            });
-        }
-    });
+    fetch("/subscription/config/")
+        .then((result) => { return result.json(); })
+        .then((data) => {
+            // Initialize Stripe.js
+            const stripe = Stripe(data.publicKey);
+            // Get Checkout Session ID
+            fetch("/subscription/create-checkout-session/?price=" + price)
+                .then((result) => {
+                    return result.json();
+                })
+                .then((data) => {
+                    console.log(data);
+                    // Redirect to Stripe Checkout
+                    return stripe.redirectToCheckout({sessionId: data.sessionId})
+                })
+                .then((res) => {
+                    console.log(res);
+                });
+        });
+}
 
