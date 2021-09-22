e9a6e2831f18661f11cbbcf45328b315c54bbc92
cjstein <resume@cjstein.com>
2021-09-21 20:47:10 -0500

    updated with sourcery

--- journal_app/users/tests/factories.py
+++ journal_app/users/tests/factories.py
@@ -5,17 +5,13 @@ from factory.django import DjangoModelFactory
 
 
 class UserFactory(DjangoModelFactory):
-
     username = Faker("user_name")
     email = Faker("email")
     name = Faker("name")
 
     @post_generation
     def password(self, create: bool, extracted: Sequence[Any], **kwargs):
-        password = (
-            extracted
-            if extracted
-            else Faker(
+        password = extracted or Faker(
                 "password",
                 length=42,
                 special_chars=True,
@@ -23,7 +19,6 @@ class UserFactory(DjangoModelFactory):
                 upper_case=True,
                 lower_case=True,
             ).generate(extra_kwargs={})
-        )
         self.set_password(password)
 
     class Meta:
