2514e4bd5b9756dfac5b9c02cecf1d5efe2901c4
cjstein <resume@cjstein.com>
2021-09-21 21:01:11 -0500

    created special case for settings.debug for testing

--- journal_app/users/signals.py
+++ journal_app/users/signals.py
@@ -1,4 +1,6 @@
 import stripe
+from string import ascii_letters
+from random import choice
 from allauth.account.signals import email_confirmed
 from django.db.models.signals import post_save
 from django.conf import settings
@@ -12,11 +14,16 @@ def create_stripe_customer(sender, instance, created, **kwargs):
     stripe.api_key = settings.STRIPE_SECRET_KEY
     if created:
         customer = StripeCustomer.objects.create(user=instance)
-        stripe_customer = stripe.Customer.create(
-            email=instance.email,
-            description=str(instance),
-        )
-        customer.stripe_customer_id = stripe_customer.stripe_id
+        if settings.DEBUG:
+            choices = ''.join(str(i) for i in range(10)) + ascii_letters
+            random_customer_string = ''.join(choice(choices) for i in range(14))
+            customer.stripe_customer_id = f'cus_{random_customer_string}'
+        else:
+            stripe_customer = stripe.Customer.create(
+                email=instance.email,
+                description=str(instance),
+            )
+            customer.stripe_customer_id = stripe_customer.stripe_id
         customer.save()
 
 
