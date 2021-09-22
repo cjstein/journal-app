2903ad9893c859cd4d8057d333d6ca2cdb87fb56
cjstein <resume@cjstein.com>
2021-09-21 21:10:53 -0500

    reverted back

--- journal_app/users/signals.py
+++ journal_app/users/signals.py
@@ -1,6 +1,4 @@
 import stripe
-from string import ascii_letters
-from random import choice
 from allauth.account.signals import email_confirmed
 from django.db.models.signals import post_save
 from django.conf import settings
@@ -14,16 +12,11 @@ def create_stripe_customer(sender, instance, created, **kwargs):
     stripe.api_key = settings.STRIPE_SECRET_KEY
     if created:
         customer = StripeCustomer.objects.create(user=instance)
-        if settings.DEBUG:
-            choices = ''.join(str(i) for i in range(10)) + ascii_letters
-            random_customer_string = ''.join(choice(choices) for i in range(14))
-            customer.stripe_customer_id = f'cus_{random_customer_string}'
-        else:
-            stripe_customer = stripe.Customer.create(
-                email=instance.email,
-                description=str(instance),
-            )
-            customer.stripe_customer_id = stripe_customer.stripe_id
+        stripe_customer = stripe.Customer.create(
+            email=instance.email,
+            description=str(instance),
+        )
+        customer.stripe_customer_id = stripe_customer.stripe_id
         customer.save()
 
 
